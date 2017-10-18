fd5d86d7a6e8123c002033058450efebbdbabcbe
angrishprerna <pangrish@uwaterloo.ca>
2017-10-18 16:33:17 -0400

    Integrate blackboard with user interface
    Integrates blackboard with user interface via a custom Application
    subclass.

--- client/app/src/main/java/ca/uwaterloo/ece/ece651projectclient/SelectGameActivity.java
+++ client/app/src/main/java/ca/uwaterloo/ece/ece651projectclient/SelectGameActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class SelectGameActivity extends AppCompatActivity {
 
@@ -11,10 +12,23 @@ public class SelectGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_game);
+        application = (BlackboardApplication) getApplication();
+
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        EditText editGameId = (EditText) findViewById(R.id.editGameId);
+        editGameId.setText(application.getBlackboard().currentGameId().value());
     }
 
+    BlackboardApplication application;
+
     public void onPlayClick(View view){
+        EditText editGameId = (EditText) findViewById(R.id.editGameId);
+        application.getBlackboard().currentGameId().set(editGameId.getText().toString());
         Intent intent = new Intent(this, GameActivity.class);
         startActivity(intent);
     }
+
 }
