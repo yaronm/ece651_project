4970c17eb458e2de302248b3496fcabb67736431
angrishprerna <pangrish@uwaterloo.ca>
2017-11-08 10:11:14 -0500

    Update Select Game Activity for new blackboard data

--- client/app/src/main/java/ca/uwaterloo/ece/ece651projectclient/SelectGameActivity.java
+++ client/app/src/main/java/ca/uwaterloo/ece/ece651projectclient/SelectGameActivity.java
@@ -4,9 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class SelectGameActivity extends AppCompatActivity {
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +20,7 @@ public class SelectGameActivity extends AppCompatActivity {
         application = (BlackboardApplication) getApplication();
 
     }
+
     @Override
     public void onResume() {
         super.onResume();
@@ -24,11 +30,53 @@ public class SelectGameActivity extends AppCompatActivity {
 
     BlackboardApplication application;
 
-    public void onPlayClick(View view){
+    public void onJoinClick(View view) {
+        //Get GameId
         EditText editGameId = (EditText) findViewById(R.id.editGameId);
+        // Update GameId to Blackboard
         application.getBlackboard().currentGameId().set(editGameId.getText().toString());
+        //Change Game state to Join Game
+        application.getBlackboard().gameState().set(GameState.JOINING);
+        //Start Game Activity
         Intent intent = new Intent(this, GameActivity.class);
         startActivity(intent);
+
     }
 
+    public void onCreateGameClick(View view) {
+        //Get NoOfPlayers
+        EditText editNoOfPlayers = (EditText) findViewById(R.id.editNoOfPlayers);
+        String value = editNoOfPlayers.getText().toString();
+        // Update NumberOfPlayers to Blackboard
+        application.getBlackboard().numberOfPlayers().set(Integer.parseInt(value));
+        switch (radioButton.getId()) {
+            case R.id.radioTypeAssassin:
+                application.getBlackboard().visibilityMatrixType().set(VisibilityMatrixType.ASSASSIN);
+                break;
+            case R.id.radioTypeHideNSeek:
+                application.getBlackboard().visibilityMatrixType().set(VisibilityMatrixType.HIDE_N_SEEK);
+                break;
+            case R.id.radioTypeCustom:
+                application.getBlackboard().visibilityMatrixType().set(VisibilityMatrixType.CUSTOM);
+                break;
+        }
+        //Change Game state to Create Game
+        application.getBlackboard().gameState().set(GameState.CREATING);
+        //Start Game Activity
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void onClick(View v) {
+        radioGroup= (RadioGroup)findViewById(R.id.radioGroupSelectMatrixType);
+
+        // get selected radio button from radioGroup
+        int selectedId = radioGroup.getCheckedRadioButtonId();
+
+        // find the radio button by returned id
+        radioButton = (RadioButton) findViewById(selectedId);
+
+    }
 }
+
