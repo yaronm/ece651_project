2f8aed25345d0eee962a7e952f9fbec35d031516
C. Ansel Horn <cahorn@uwaterloo.ca>
2017-11-12 14:02:23 -0500

    Initialize firebase unit testing
    Initializes FirebaseCommunicationTest class for unit testing firebase
    without a network connection to the firebase server.

---
+++ client/app/src/androidTest/java/ca/uwaterloo/ece/ece651projectclient/FirebaseCommunicationTest.java
@@ -0,0 +1,78 @@
+package ca.uwaterloo.ece.ece651projectclient;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * FirebaseCommunication offline unit testing. Does not include online, server integration testing.
+ */
+@RunWith(AndroidJUnit4.class)
+public class FirebaseCommunicationTest {
+
+    @BeforeClass
+    public static void setUpClass() {
+        // turn firebase client networking off for testing
+        FirebaseDatabase.getInstance().goOffline();
+    }
+
+    @Before
+    public void setUp() {
+        // initialize the firebase client to a base testing configuration
+        // (to avoid data persistence between tests)
+        database = FirebaseDatabase.getInstance().getReference();
+        database.removeValue();
+        // initialize the blackboard
+        blackboard = new ConcreteBlackboard();
+        // initialize the firebase communication module
+        communication = new FirebaseCommunication(blackboard);
+        // use the firebase connection to setup a test user
+        blackboard.userName().set("testuser");
+    }
+
+    DatabaseReference database;
+    Blackboard blackboard;
+    FirebaseCommunication communication;
+
+    @Test
+    public void testUserNull() {
+        // test that the program does not crash on a null username
+        blackboard.userName().set(null);
+    }
+
+    @Test
+    public void testUserNew() {
+        // set the username to a new user
+        String username = "newuser";
+        blackboard.userName().set(username);
+        BlockingValueEventListener listener = new BlockingValueEventListener();
+        database.child("users").addListenerForSingleValueEvent(listener);
+        // test that a new user exists
+        assertEquals(2, listener.getSnapshot().getChildrenCount());
+        // test that the correct new user was added
+        assertNotNull(listener.getSnapshot().child(username));
+    }
+
+    @Test
+    public void testUserReturning() {
+        // set the username to a returning user
+        String username = "testuser";
+        blackboard.userName().set(username);
+        BlockingValueEventListener listener = new BlockingValueEventListener();
+        database.child("users").addListenerForSingleValueEvent(listener);
+        // test that a no new users have been created
+        assertEquals(1, listener.getSnapshot().getChildrenCount());
+        // test that the returning user still exists
+        assertNotNull(listener.getSnapshot().child(username));
+    }
+
+}
