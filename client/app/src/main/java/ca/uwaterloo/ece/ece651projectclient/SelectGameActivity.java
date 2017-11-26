846ea00d94588dd5e9934571d2f66bf43a7f2b1c
angrishprerna <pangrish@uwaterloo.ca>
2017-11-26 11:06:04 -0500

    Added scroll view and fixed game creation bugs

--- client/app/src/main/java/ca/uwaterloo/ece/ece651projectclient/SelectGameActivity.java
+++ client/app/src/main/java/ca/uwaterloo/ece/ece651projectclient/SelectGameActivity.java
@@ -3,12 +3,15 @@ package ca.uwaterloo.ece.ece651projectclient;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -35,7 +38,7 @@ public class SelectGameActivity extends AppCompatActivity {
                 List<String> availableGameList = new ArrayList<>(
                         application.getBlackboard().availableGames().value());
                 ArrayAdapter<String> adapter = new ArrayAdapter<String>(SelectGameActivity.this,
-                                android.R.layout.simple_spinner_item, availableGameList);
+                        android.R.layout.simple_spinner_item, availableGameList);
                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 Spinner availableGames = (Spinner) findViewById(R.id.spinnerAvailableGames);
                 availableGames.setAdapter(adapter);
@@ -68,12 +71,30 @@ public class SelectGameActivity extends AppCompatActivity {
         //Get otherNames
         EditText editOtherNames = (EditText) findViewById(R.id.editOtherNames);
         Set<String> names = new HashSet<String>(Arrays.asList(editOtherNames.getText().toString().split(",")));
-        application.getBlackboard().othersNames().set(names);
         //Get NoOfPlayers
         EditText editNoOfPlayers = (EditText) findViewById(R.id.editNoOfPlayers);
         String value = editNoOfPlayers.getText().toString();
-        // Update NumberOfPlayers to Blackboard
-        application.getBlackboard().numberOfPlayers().set(Integer.parseInt(value));
+        //check if either of those two are given
+        int check = 0;
+        if (TextUtils.isEmpty(value) && !TextUtils.isEmpty(editOtherNames.getText().toString())) {
+            //Update otherNames to blackboard
+            application.getBlackboard().othersNames().set(names);
+            check = 1;
+        } else if (!TextUtils.isEmpty(value) && TextUtils.isEmpty(editOtherNames.getText().toString())) {
+            // Update NumberOfPlayers to Blackboard
+            application.getBlackboard().numberOfPlayers().set(Integer.parseInt(value));
+            check = 1;
+        } else if (!TextUtils.isEmpty(value) && !TextUtils.isEmpty(editOtherNames.getText().toString())) {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "Enter either other users of number of players", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+            toast.show();
+        } else if (TextUtils.isEmpty(value) && TextUtils.isEmpty(editOtherNames.getText().toString())) {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "Enter either other users of number of players", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+            toast.show();
+        }
         switch (radioButton.getId()) {
             case R.id.radioTypeAssassin:
                 application.getBlackboard().visibilityMatrixType().set(VisibilityMatrixType.ASSASSIN);
@@ -88,13 +109,15 @@ public class SelectGameActivity extends AppCompatActivity {
         //Change Game state to Create Game
         application.getBlackboard().gameState().set(GameState.CREATING);
         //Start Game Activity
-        Intent intent = new Intent(this, GameActivity.class);
-        startActivity(intent);
+        if (check == 1) {
+            Intent intent = new Intent(this, GameActivity.class);
+            startActivity(intent);
+        }
 
     }
 
     public void onClick(View v) {
-        radioGroup= (RadioGroup)findViewById(R.id.radioGroupSelectMatrixType);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroupSelectMatrixType);
 
         // get selected radio button from radioGroup
         int selectedId = radioGroup.getCheckedRadioButtonId();
